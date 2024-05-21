b612e6ddfe3106b96b322922b2fff1c2cd831738
colbycheese55 <112818133+colbycheese55@users.noreply.github.com>
2024-05-21 17:48:19 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,81 @@
+import json
+import cantools as ct
+import random as rand
+import math
+import os
+import sys
+
+dbc_files = os.listdir("./CAN-messages")
+dbs = [ct.db.load_file(f"./CAN-messages/{file}") for file in dbc_files if file.endswith(".dbc")]
+
+file_map = dict()
+message_map = dict()
+
+
+def setup_maps() -> None:
+    for template in config:
+        handled = False
+        for db in dbs:
+            for message in db.messages:
+                if message.name == template:
+                    message_map[template] = message
+                    file_map[template] = db
+                    handled = True
+                    break
+        if not handled:
+            print(f"ERROR:Message type \"{template}\" not found in any DBC file.")
+            exit(1)
+
+
+def generate_message(message_type: str, data: dict) -> str:
+    db = file_map[message_type]
+    message = message_map[message_type]
+
+    default_data = {signal.name: 0 for signal in message.signals}
+    data = default_data | data
+
+    try:
+        encoded_message_bytes = db.encode_message(message_type, data)
+    except Exception as e:
+        print(f"Error encoding message: {e}")
+        exit(1)
+    encoded_message_hex = encoded_message_bytes.hex()
+    while len(encoded_message_hex) < message.length * 2:
+        encoded_message_hex = "0" + encoded_message_hex
+    id_hex = hex(message.frame_id)[2:]
+
+    text = f"00:00:00 DEBUG /root/Rivanna2/Common/src/MainCANInterface.cpp:40: Sent CAN message with ID 0x{id_hex} Length {message.length} Data 0x{encoded_message_hex}"
+    return text
+
+
+def main() -> None:
+    if len(sys.argv) != 3:
+        print("ERROR: Expected 2 command line arguments: config json file path, number of messages to generate")
+        exit(1)
+    config_path = sys.argv[1]
+    if not os.path.exists(config_path):
+        print(f"ERROR: File \"{config_path}\" not found.")
+        exit(1)
+    with open(config_path, "r") as file:
+        global config
+        config = json.load(file)
+    num_messages = int(sys.argv[2])
+
+    setup_maps()
+    templates_keys = list(config.keys())
+    
+    with open("out.txt", "w") as file:
+        for i in range(num_messages):
+            messageType = rand.choice(templates_keys)
+            template = config[messageType]
+            try:
+                messageData = {key: eval(str(template[key]), None, {"i": i}) for key in template.keys()}
+            except Exception as e:
+                print(f"Error when evaluating a statement in {config_path} for {messageType}: \n{e}")
+                exit(1)
+            text = generate_message(messageType, messageData)
+            file.write(text+"\n")
+
+# TODO implement timestamps
+if __name__ == '__main__':
+    main()
